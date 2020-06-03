3142be6f86751b1611926c2976f4c8a50e3396ed
AlanVazquez99 <alanvazquez1999@gmail.com>
2020-06-02 23:07:39 -0500

    Integracion de test

--- run.sh
+++ run.sh
@@ -5,4 +5,12 @@ export FLASK_APP=main.py
 export FLASK_DEBUG=1
 export FLASK_ENV=development
 
-flask run
+echo "Elija la opcion a ejecutar"
+echo -ne "Test App[t] 		Correr App[r]: \t\t"
+read option
+
+if [ "$option" == "t" ]; then
+	flask test
+else 
+	flask run
+fi

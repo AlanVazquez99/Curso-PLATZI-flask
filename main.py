4557fd28a9d10108c94d4240bba821d4811b407f
AlanVazquez99 <alanvazquez1999@gmail.com>
2020-06-02 20:44:46 -0500

    Formulario de Inicio de sesion

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, request, make_response, redirect, render_template, abort
+from flask import Flask, request, make_response, redirect, render_template, abort, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
-bootstrap =Bootstrap(app)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    SubmitField = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
@@ -27,17 +39,23 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    login_form = LoginForm()
     contex = {
-        'user_ip': user_ip,
-        'todos': todos
+        'user_ip': session.get('user_ip'),
+        'todos': todos,
+        'login_form': login_form,
+        'username': session.get('username'),
     }
+
+    if login_form.validate_on_submit():
+        session['username'] = login_form.username.data
+        return redirect('/')
 
     return render_template('hello.html', **contex)
